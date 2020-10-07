94f1e44603c1bbec6e15943b522e34020e11f41e
DANIEL JAMES <daniel.james@codeimmersives.com>
2020-10-07 15:25:21 -0400

    updated timer output

--- app.js
+++ app.js
@@ -48,4 +48,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
+    
 })     
--- middlewares/timer.js
+++ middlewares/timer.js
@@ -2,7 +2,8 @@ const moment = require('moment')
 function timer(req, res, next) {
 
     // console.log(req.method, req.url, res.statusCode)
-    console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+    // console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+    console.log(moment().format('lll'))
 
     next();
 }
